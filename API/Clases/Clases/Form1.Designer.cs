46398e9bc48558fae40302bfb8216b7eca13f139
NubiaMartinez <118578334+NubiaMartinez@users.noreply.github.com>
2023-04-04 17:03:10 -0600

    Add files via upload

---
+++ API/Clases/Clases/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Clases
+{
+    partial class Apiii
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNombreGET = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellidoGET = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTrabajador = new System.Windows.Forms.TextBox();
+            this.txtNombresPost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.BackColor = System.Drawing.Color.White;
+            this.btnPrueba.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrueba.ForeColor = System.Drawing.Color.Black;
+            this.btnPrueba.Location = new System.Drawing.Point(349, 37);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(63, 53);
+            this.btnPrueba.TabIndex = 0;
+            this.btnPrueba.Text = "GET";
+            this.btnPrueba.UseVisualStyleBackColor = false;
+            this.btnPrueba.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // btnPost
+            // 
+            this.btnPost.BackColor = System.Drawing.Color.White;
+            this.btnPost.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPost.Location = new System.Drawing.Point(168, 256);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(87, 53);
+            this.btnPost.TabIndex = 2;
+            this.btnPost.Text = "POST";
+            this.btnPost.UseVisualStyleBackColor = false;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(93, 13);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(237, 27);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtNombreGET
+            // 
+            this.txtNombreGET.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreGET.Location = new System.Drawing.Point(93, 47);
+            this.txtNombreGET.Name = "txtNombreGET";
+            this.txtNombreGET.Size = new System.Drawing.Size(237, 27);
+            this.txtNombreGET.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtApellidoGET
+            // 
+            this.txtApellidoGET.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellidoGET.Location = new System.Drawing.Point(93, 81);
+            this.txtApellidoGET.Name = "txtApellidoGET";
+            this.txtApellidoGET.Size = new System.Drawing.Size(237, 27);
+            this.txtApellidoGET.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Apellido:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTrabajador);
+            this.groupBox1.Controls.Add(this.txtNombresPost);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 104);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agrega Trabajador:";
+            // 
+            // txtTrabajador
+            // 
+            this.txtTrabajador.Location = new System.Drawing.Point(112, 27);
+            this.txtTrabajador.Name = "txtTrabajador";
+            this.txtTrabajador.Size = new System.Drawing.Size(225, 27);
+            this.txtTrabajador.TabIndex = 13;
+            // 
+            // txtNombresPost
+            // 
+            this.txtNombresPost.Location = new System.Drawing.Point(112, 62);
+            this.txtNombresPost.Name = "txtNombresPost";
+            this.txtNombresPost.Size = new System.Drawing.Size(225, 27);
+            this.txtNombresPost.TabIndex = 11;
+            this.txtNombresPost.TextChanged += new System.EventHandler(this.txtNombresPost_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Trabajador:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nombre:";
+            // 
+            // Apiii
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(434, 311);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtApellidoGET);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombreGET);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.btnPrueba);
+            this.Name = "Apiii";
+            this.Text = "Ejercicio1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrueba;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNombreGET;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellidoGET;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTrabajador;
+        private System.Windows.Forms.TextBox txtNombresPost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ API/Clases/Clases/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Clases.ApiRest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clases
+{
+    public partial class Apiii : Form
+    {
+        APII dBApi = new APII();
+        public Apiii()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            dynamic respuesta = dBApi.Get("https://reqres.in/api/users?page=1");
+            pictureBox1.ImageLocation = respuesta.data[1].avatar.ToString();
+            txtNombreGET.Text = respuesta.data[1].first_name.ToString();
+            txtApellidoGET.Text = respuesta.data[1].last_name.ToString();
+            txtEmail.Text = respuesta.data[1].email.ToString();
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona
+            {
+                job = txtTrabajador.Text,
+                name = txtNombresPost.Text
+            };
+
+            string json = JsonConvert.SerializeObject(persona);
+
+            dynamic respuesta = dBApi.Post("https://reqres.in/api/users",json);
+
+            MessageBox.Show(respuesta.ToString());
+        }
+
+        private void txtNombresPost_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Persona
+    {
+        public string name { get; set; }
+        public string job { get; set; }
+    }
+}
+
